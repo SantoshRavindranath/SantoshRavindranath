956c8428c9f87880bf9d177ce1b2dfc80e28a143
Santosh-Ravindranath <santosh.ravindranath@a-cti.com>
2016-02-28 19:53:47 +0530

    first commit

---
+++ Javtest/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test
+	{
+public static void main(String args[])
+{
+	String a="Punitha";
+	String b=new String("Vathy");
+	
+	System.out.println(a== b);
+}
+	}
---
+++ test/src/test/Testclass.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Testclass
+	{
+public static void main(String args[])
+{
+	int i=0;
+	while (i<4)
+		{
+			i++;
+			if(i==3)
+				{
+				continue;
+				}
+			System.out.println(i);
+		}
+}
+	}
